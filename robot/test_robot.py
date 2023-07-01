8556a4620f94264cc1a577f15609e2e17e958f76
Albert <albert.perello.covas@icloud.com>
2023-07-01 21:48:45 +0200

    added function send_photo

--- robot/test_robot.py
+++ robot/test_robot.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv() #Load .env variables
+api_token = os.getenv("API_TOKEN") #Get token api from .env file
+chat_id = os.getenv("CHAT_ID") #Get chat id from .env file
 
-def send_to_telegram(message):
+def send_to_telegram(message, apiToken, chatID):
 
-    apiToken = os.getenv("API_TOKEN") #Get token api from .env file
-    chatID = os.getenv("CHAT_ID") #Get chat id from .env file
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
 
     try:
@@ -16,4 +16,14 @@ def send_to_telegram(message):
     except Exception as e:
         print(e)
 
-send_to_telegram("Hola desde python!")
+def send_photo(chatID, file_opened, apiToken):
+    telegramToken = apiToken
+    api_url=f'https://api.telegram.org/bot{telegramToken}/'
+    method = "sendPhoto"
+    params = {'chat_id': chatID}
+    files = {'photo': file_opened}
+    resp = requests.post(api_url + method, params, files=files)
+    return resp
+
+send_to_telegram(message="Hola desde python!", apiToken=api_token, chatID=chat_id)
+send_photo(chatID=chat_id, apiToken=api_token, file_opened=open('./robot/test.png', 'rb'))
